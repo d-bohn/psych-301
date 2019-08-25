bdb74439210da78964199aa294591be843c6fafc
Daniel Albohn <dna5021@psu.edu>
2019-08-25 15:29:16 -0400

    initial commit

---
+++ docs/R/render_site.R
@@ -0,0 +1,13 @@
+files <- list.files("lessons", pattern = "*.Rmd$", full.names = TRUE)
+
+# purrr::walk(files, rmarkdown::render, output_format = "html_document")
+
+for (f in files) rmarkdown::render(f)
+
+files_html <- stringr::str_replace_all(files, ".Rmd", ".html")
+
+purrr::walk(c(files,files_html), fs::file_copy,
+            new_path = "docs/lessons", overwrite = TRUE)
+
+rmarkdown::render_site()
+
